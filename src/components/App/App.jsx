d5a45707738e1dbb1f9b0dc5d40c30444747714f
Ben Capp <bentucker333@gmail.com>
2023-02-16 16:46:45 -0600

    built GET in app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+  const [galleryList, setGalleryList] = useState([]);
+
+  useEffect(() => {
+    getGallery();
+  }, []);
+
+  // Client GET
+  const getGallery = () => {
+    axios
+      .get("/gallery")
+      .then((response) => {
+        setGalleryList(response.data);
+      })
+      .catch((err) => console.log("Error in client GET:", err));
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <p>Gallery goes here</p>
+      <img src="images/goat_small.jpg" />
+    </div>
+  );
 }
 
 export default App;
